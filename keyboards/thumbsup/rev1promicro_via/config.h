729f73b1a9bdee2c392c7fda63114e242f1f2607
ak66666 <32971450+ak66666@users.noreply.github.com>
2023-01-04 19:06:36 -0500

    Added REMAP support for ProMicro/VIA

--- keyboards/thumbsup/rev1promicro_via/config.h
+++ keyboards/thumbsup/rev1promicro_via/config.h
@@ -1,38 +1,47 @@
+/*
+Copyright 2019 Bubnick
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #pragma once
 
+#include "config_common.h"
 
 
-#define TAPPING_TERM 170 //170 // 200 //120 //00 //90 //200 // how long before a tap becomes a hold
+/* USB Device descriptor parameter */
+/* Deprecated, is specified in the JSON file*/
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x0000 
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    Alexander (Sasha) Karmanov
+#define PRODUCT         Thumbs Up!
+/* */
 
+/* key matrix size */
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 12
 
-
-//#define RETRO_TAPPING
-//#define INGNORE_MOD_TAP_INTERRUPT
-//#undef PERMISSIVE_HOLD                                                                                                  
-//#define  PREVENT_STUCK_MODIFIERS
-
-
-#define MOUSEKEY_DELAY             150 //200
-#define MOUSEKEY_INTERVAL          60 //50
-#define MOUSEKEY_MAX_SPEED         5  //10
-#define MOUSEKEY_TIME_TO_MAX       7 //20
-#define MOUSEKEY_WHEEL_MAX_SPEED   5	//8
-#define MOUSEKEY_WHEEL_TIME_TO_MAX 40
+/* key matrix pins */
+#define MATRIX_ROW_PINS { D3, D2, D4, C6 }
+#define MATRIX_COL_PINS { B2, B6, B5, B4, E6, D7, B3, F4, F5, F6, F7, B1 }
  
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
 
-#define USB_POLLING_INTERVAL_MS 1
 
 
-#define POINTING_DEVICE_ROTATION_90
-#define CIRQUE_PINNACLE_TAP_ENABLE
-#define CIRQUE_PINNACLE_SECONDARY_TAP_ENABLE
-#define POINTING_DEVICE_GESTURES_SCROLL_ENABLE
-
-//#define CIRQUE_PINNACLE_ATTENUATION ADC_ATTENUATE_2X
-//#define CIRQUE_PINNACLE_POSITION_MODE CIRQUE_PINNACLE_RELATIVE_MODE
-#define CIRQUE_PINNACLE_POSITION_MODE CIRQUE_PINNACLE_ABSOLUTE_MODE
-
-//#define CIRQUE_PINNACLE_SECONDARY_TAP_ENABLE
-//#define POINTING_DEVICE_GESTURES_SCROLL_ENABLE
-//#define POINTING_DEVICE_GESTURES_CURSOR_GLIDE_ENABLE 
-
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
