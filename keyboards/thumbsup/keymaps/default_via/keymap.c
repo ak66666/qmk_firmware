988d993f82b7ff964035b8f7bd9d5d1b7e080431
ak66666 <32971450+ak66666@users.noreply.github.com>
2023-03-13 20:27:20 -0400

    Added rev1 via option

--- keyboards/thumbsup/keymaps/default_via/keymap.c
+++ keyboards/thumbsup/keymaps/default_via/keymap.c
@@ -55,7 +55,7 @@ Single hit:		       |      |Space ||Space |      |
   KC_ESC,                  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,       KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC, \
   KC_TAB, 		   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,       KC_J,    KC_K,    KC_L,   KC_SCLN, KC_ENT , \
   KC_LCTL,                 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,       KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RCTL, \
-  XXXXXXX,                 XXXXXXX, KC_LGUI, KC_LALT, KC_LSFT, KC_SPC,     KC_SPC, KC_RSFT, KC_RALT, KC_APP, XXXXXXX, XXXXXXX  \
+    				    KC_LGUI, KC_LALT, KC_LSFT, KC_SPC,     KC_SPC, KC_RSFT, KC_RALT, KC_APP \
 ),
 
 
@@ -109,7 +109,7 @@ without separate thumbcluster.
   _______, KC_BRK,           KC_WH_D,      KC_MS_U,      KC_WH_U,      KC_DEL,              KC_PGUP,  KC_HOME, KC_UP,   KC_END,  KC_GRV,  KC_DEL,  \
   KC_ENT,  LCTL(KC_A),       KC_MS_L,      KC_MS_D,      KC_MS_R,      XXXXXXX,             KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, _______, \
   _______, LCTL(KC_Z),       LSFT(KC_DEL), LCTL(KC_INS), LSFT(KC_INS), XXXXXXX,             KC_INS,   KC_BTN1, KC_BTN3, KC_BTN2, XXXXXXX, _______, \
-  XXXXXXX, XXXXXXX,          _______,       _______, _______, _______,       _______, _______,      _______,      _______,      XXXXXXX, XXXXXXX  \
+			     _______,       _______, _______, _______,       _______, _______,      _______,      _______ \
 ),
 
 
@@ -129,7 +129,7 @@ without separate thumbcluster.
   _______, KC_BRK,          KC_HOME, KC_UP,   KC_END,  KC_PGUP,       KC_DEL,  KC_WH_U,      KC_MS_U,      KC_WH_D,      KC_GRV,  KC_DEL , \
   KC_ENT,  LCTL(KC_A),      KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,       XXXXXXX, KC_MS_L,      KC_MS_D,      KC_MS_R,      XXXXXXX, _______, \
   _______, XXXXXXX,         KC_BTN2, KC_BTN3, KC_BTN1, KC_INS,        XXXXXXX, LCTL(KC_DEL), LCTL(KC_INS), LSFT(KC_INS), XXXXXXX, _______, \
-  XXXXXXX, XXXXXXX,         _______, _______, _______, _______,       _______, _______,      _______,      _______,      XXXXXXX, XXXXXXX  \
+			    _______, _______, _______, _______,       _______, _______,      _______,      _______ 	\
 
 ),
 
@@ -150,7 +150,7 @@ without separate thumbcluster.
   _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_TAB,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
   _______, KC_F11,  KC_F12,  KC_LALT, KC_LSFT, KC_LALT, KC_RALT, KC_RSFT, KC_COMM, KC_DOT,  KC_SLSH, _______, \
-  XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX  \
+ 		    _______, _______, _______, _______, _______, _______, _______, _______	 \
 )
 };
 
--- keyboards/thumbsup/keymaps/default_w_numpad/keymap.c
+++ keyboards/thumbsup/keymaps/default_w_numpad/keymap.c
@@ -214,7 +214,7 @@ Single hit:		       |      |Space ||Space |      |
  */
 [_NUMPAD] =  LAYOUT_ortho_4x12( \
   KC_ESC,                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_KP_SLASH, KC_7,    KC_8, KC_9, KC_KP_MINUS, KC_BSPC, \
-  LT(_EXTRARIGHT,KC_TAB), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_KP_PAST,  KC_4,    KC_5, KC_6, KC_KP_PLUS,  KC_PENT, \
+  LT(_EXTRARIGHT,KC_TAB), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_PAST,     KC_4,    KC_5, KC_6, KC_KP_PLUS,  KC_PENT, \
   KC_LCTL,                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_COMM,     KC_1,    KC_2, KC_3, KC_DOT,      KC_RCTL, \
                                    KC_LGUI, KC_LALT, KC_LSFT, CURSORLEFT, CURSORRGHT,  KC_RSFT, KC_0, KC_DOT                      \
 ),
